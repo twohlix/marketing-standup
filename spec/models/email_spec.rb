c2bef1c82bb634d7ebdcd9de733c743c66e9160f
Charles Smith <charles@balconyinfive.com>
2014-03-05 19:42:54 -0500

    added confirmation specs to emails

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -53,4 +53,43 @@ describe Email do
       expect(@last_email.address).to eq(@last_email.address.downcase)
     end
   end
+
+  context "confirmation" do
+    before(:each) do
+      @new_email = Email.new
+      @new_email.address = 'confirm@example.com'
+    end
+
+    it "has not been confirmed by default" do
+      expect(@new_email.confirmed?).to eq(false)
+    end
+
+    it "responds to confirm" do
+      expect(@new_email.respond_to? :confirm).to eq(true)
+    end
+
+    it "does not confirm if the object is not persisted" do
+      expect(@new_email.persisted?).to eq(false)
+      expect(@new_email.confirm).to eq(false)
+      @new_email.save
+      expect(@new_email.confirm).to eq(true)
+    end
+
+    it "creates a confirmation key when asked to confirm" do
+      expect(@new_email.confirmation_key).to eq(nil)
+      @new_email.save
+      expect(@new_email.confirm).to eq(true)
+      expect(@new_email.confirmation_key).to_not eq(nil)
+    end
+
+    it "generates a new confirmation key every time" do
+      @new_email.save
+      @new_email.confirm
+      old_key = @new_email.confirmation_key
+      @new_email.confirm
+      expect(@new_email.confirmation_key).to_not eq(old_key)
+      expect(old_key).to_not eq(nil)
+      expect(@new_email.confirmation_key).to_not eq(nil)
+    end
+  end
 end
