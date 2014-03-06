04a57e8c5795500db40ec50271c223461a82e27a
Charles Smith <charles@balconyinfive.com>
2014-03-05 20:42:05 -0500

    Refactored the email spec and added more thorough confirmation testing

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -60,36 +60,81 @@ describe Email do
       @new_email.address = 'confirm@example.com'
     end
 
+    it "responds to confirmed?" do
+      expect(@new_email.respond_to? :confirmed?).to eq(true)
+    end
+
     it "has not been confirmed by default" do
       expect(@new_email.confirmed?).to eq(false)
     end
 
-    it "responds to confirm" do
-      expect(@new_email.respond_to? :confirm).to eq(true)
+    describe "send_confirmation" do
+      it "responds to send_confirmation" do
+        expect(@new_email.respond_to? :send_confirmation).to eq(true)
+      end
+
+      it "has 0 confirmation attempts by default" do
+        expect(@new_email.confirmation_attempts).to eq(0)
+      end
+
+      it "does not send confirmations if the object is not persisted" do
+        expect(@new_email.persisted?).to eq(false)
+        expect(@new_email.send_confirmation).to eq(false)
+      end
+
+      it "creates a confirmation key when sending confirmation" do
+        expect(@new_email.confirmation_key).to eq(nil)
+        @new_email.save
+        expect(@new_email.send_confirmation).to eq(true)
+        expect(@new_email.confirmation_key).to_not eq(nil)
+      end
+
+      it "generates a new confirmation key every time" do
+        @new_email.save
+        @new_email.send_confirmation
+        old_key = @new_email.confirmation_key
+        @new_email.send_confirmation
+        expect(@new_email.confirmation_key).to_not eq(old_key)
+        expect(old_key).to_not eq(nil)
+        expect(@new_email.confirmation_key).to_not eq(nil)
+      end
     end
 
-    it "does not confirm if the object is not persisted" do
-      expect(@new_email.persisted?).to eq(false)
-      expect(@new_email.confirm).to eq(false)
-      @new_email.save
-      expect(@new_email.confirm).to eq(true)
-    end
+    describe "confirm" do
+      it "responds to confirm" do
+        expect(@new_email.respond_to? :confirm).to eq(true)
+      end
 
-    it "creates a confirmation key when asked to confirm" do
-      expect(@new_email.confirmation_key).to eq(nil)
-      @new_email.save
-      expect(@new_email.confirm).to eq(true)
-      expect(@new_email.confirmation_key).to_not eq(nil)
-    end
+      it "does not confirm if the object is not persisted" do
+        expect(@new_email.persisted?).to eq(false)
+        confirm_key = @new_email.confirmation_key
+        expect(@new_email.confirm).to eq(false)
+        expect(@new_email.confirm confirm_key).to eq(false)
+      end
 
-    it "generates a new confirmation key every time" do
-      @new_email.save
-      @new_email.confirm
-      old_key = @new_email.confirmation_key
-      @new_email.confirm
-      expect(@new_email.confirmation_key).to_not eq(old_key)
-      expect(old_key).to_not eq(nil)
-      expect(@new_email.confirmation_key).to_not eq(nil)
+      it "does not confirm when given a nil/emtpy/blank key" do
+        @new_email.save
+        nil_key = nil
+        empty_key = ''
+        blank_key = '  '
+        expect(@new_email.confirm).to eq(false)
+        expect(@new_email.confirm nil_key).to eq(false)
+        expect(@new_email.confirm empty_key).to eq(false)
+        expect(@new_email.confirm blank_key).to eq(false)
+      end
+
+      it "does confirm when given the matching key" do
+        @new_email.save
+        @new_email.send_confirmation
+        @new_email.save
+
+        confirm_key = @new_email.confirmation_key
+        expect(@new_email.confirmed?).to eq(false)
+        expect(@new_email.confirm confirm_key).to eq(true)
+        expect(@new_email.confirmed?).to eq(true)
+      end
+
+      
     end
   end
 end
