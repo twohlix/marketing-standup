946c994ac77e9f8d2448d76df537f71a89222c22
Charles Smith <charles@balconyinfive.com>
2014-03-05 19:54:30 -0500

    Added basic confirmation key generation to emails

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,20 @@
 class Email < ActiveRecord::Base
+  require 'digest/md5'
+
   validates :address, presence: true, uniqueness: { case_sensitive: false }
 
   def address=(addy)
     self[:address] = addy.downcase
   end
+
+  def confirm
+    return false unless persisted?
+
+    md5 = Digest::MD5.new
+
+    self[:confirmation_key] = md5.hexdigest "#{address}#{Time.now.usec.to_s}"
+  
+    return false if changes.count > 1
+    save
+  end 
 end
